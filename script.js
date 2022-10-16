2c9be043029b98bcb360aac0f131bb1273bf79f8
TulioLeitao <tulioleitao@gmail.com>
2022-10-16 06:04:21 -0300

    conclusão do projeto

--- script.js
+++ script.js
@@ -58,46 +58,57 @@ console.log(nome7.toUpperCase(), idade6, meu6, cor6);
 const osMeusGatos =[];
 
 const meusGatos1 ={
-    nome: "Meriadoc",
+    nome: "meriadoc",
     idade: 4,
-    posse: true,
-    Cor:["Laranja"]
+    responsavel: true,
+    Cor:["Laranja"],
+    imagem: "IMG/WhatsApp Image 2022-10-15 at 20.58.44.jpeg",//para semana 11
+    link:"https://ibb.co/gJjyHmM"//para semana 11
 };
 const meusGatos2 ={
-    nome: "Billy",
+    nome: "billy",
     idade: 9,
-    posse: false,
+    responsavel: false,
     Cor:["Cinza", " Preto"]
 };
 const meusGatos3 ={
-    nome: "Peregrin",
+    nome: "peregrin",
     idade: 4,
-    posse: true,
-    Cor:["Laranja"]
+    responsavel: true,
+    Cor:["Laranja"],
+    imagem: "IMG/WhatsApp Image 2022-10-15 at 20.24.32.jpeg",//para semana 11
+    link:"https://ibb.co/f8h43pq"//para semana 11
 };
 const meusGatos4 ={
-    nome: "Mamacita",
+    nome: "mamacita",
     idade: 6,
-    posse: true,
-    Cor:["Preta", "Branca"]
+    responsavel: true,
+    Cor:["Preta", " Branca"],
+    imagem: "IMG/WhatsApp Image 2022-10-15 at 20.39.59.jpeg",//para semana 11
+    link:"https://ibb.co/Z6TnJNZ"//para semana 11
 };
 const meusGatos5 ={
-    nome: "Thomas",
+    nome: "thomas",
     idade: 2,
-    posse: false,
+    responsavel: false,
     Cor:["Cinza", " Branco"]
 };
 const meusGatos6 ={
-    nome: "Elanor",
+    nome: "elanor",
     idade: 3,
-    posse: true,
-    Cor:["Branca", "Laranja", "Preta"]
+    responsavel: true,
+    Cor:["Branca", " Laranja", " Preta"],
+    imagem: "IMG/WhatsApp Image 2022-10-15 at 20.39.55.jpeg",//para semana 11
+    link:"https://ibb.co/x7mnY08"//para semana 11
+
 };
 const meusGatos7 ={
-    nome: "Loki",
+    nome: "loki",
     idade: 1,
-    posse: true,
-    Cor:["Cinza", "Branco"]
+    responsavel: true,
+    Cor:["Cinza", " Branco"],
+    imagem: "IMG/WhatsApp Image 2022-10-15 at 20.40.04.jpeg",//para semana 11
+    link:"https://ibb.co/t31XkWL"//para semana 11
 };
 /* 
 osMeusGatos.push(meusGatos1, meusGatos2, meusGatos3, meusGatos4, meusGatos5, meusGatos6, meusGatos7); //retirado para a semana 4  
@@ -120,37 +131,37 @@ console.log(Object.entries(meusGatos1));
 
 
 // estou trocando o *ALERT* po um *CONSOLE.LOG* para ficar mais fácil de trabalhar a página. 
-if(meusGatos1.posse){
+if(meusGatos1.responsavel){
     osMeusGatos.push(meusGatos1)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
 };
-if(meusGatos2.posse){
+if(meusGatos2.responsavel){
     osMeusGatos.push(meusGatos2)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
 };
-if(meusGatos3.posse){
+if(meusGatos3.responsavel){
     osMeusGatos.push(meusGatos3)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
 };
-if(meusGatos4.posse){
+if(meusGatos4.responsavel){
     osMeusGatos.push(meusGatos4)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
 };
-if(meusGatos5.posse){
+if(meusGatos5.responsavel){
     osMeusGatos.push(meusGatos5)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
 };
-if(meusGatos6.posse){
+if(meusGatos6.responsavel){
     osMeusGatos.push(meusGatos6)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
 };
-if(meusGatos7.posse){
+if(meusGatos7.responsavel){
     osMeusGatos.push(meusGatos7)
 } else {
     console.log("Esse gato não é meu! Deixa de ser doido e devolve para o dono!")
@@ -197,17 +208,27 @@ for (gato of osMeusGatos) {
 //---------------------Semana 6-------------
 
 //parte 1
-const arrayDeGatos = osMeusGatos;
-
+/*const arrayDeGatos = osMeusGatos;
 const gatosNoMap = arrayDeGatos.map((gato)=>{
     return `O(A) gato(a) ${gato.nome} tem ${gato.idade} anos, e é alem de lindo(a) é ${gato.Cor}.`;
    
 });
 
-console.log(gatosNoMap);
+console.log(gatosNoMap);*/
+
+function retornaObejto (objeto){
+    let stringObjeto = ``
+    stringObjeto = objeto
+    console.log(stringObjeto)
+};
+
+retornaObejto(meusGatos1);
+
+
 
 //parte 2
 
+/* isso foi o que eu achei que havia sido pedido 
 const soOsLaranajs = arrayDeGatos.filter( (gato, index, array) => {
     let resultado = false;
     if(gato.Cor === "Laranja"){
@@ -217,8 +238,104 @@ const soOsLaranajs = arrayDeGatos.filter( (gato, index, array) => {
         console.log("Esse gato não é laranja!")
     };
         return resultado;
-});
+});*/
 
-console.log(soOsLaranajs);
+const funcaoDeBusca = (arrayObj, input) => {
+    let buscaObejto = {}
+    for (let i in arrayObj) {
+        for (let j in arrayObj[i]) {
+            if (arrayObj[i][j]=== input) {
+                    let main = document.getElementById("main"); // semana 12
+                    let div = document.createElement('div');
+                    let section = document.createElement("section");
+                    let imagem = document.createElement("img");
+                    let ul = document.createElement("ul");
+                    let li1 = document.createElement ("li");
+                    let li2 = document.createElement ("li");
+                    let li3 = document.createElement ("li");
+                    let li4 = document.createElement ("li");
+                    let a = document.createElement ("a");
+                    div.setAttribute("id", "div-sections");
+                    section.setAttribute("id","section1");
+                    imagem.src = arrayObj[i].imagem;
+                    a.setAttribute("href", arrayObj[i].link);
+                    a.setAttribute("target","_blank");
+                    a.innerHTML = arrayObj[i].nome.toUpperCase()
+                    li1.innerHTML = `Nome: `
+                    li2.innerHTML = `Idade: ${arrayObj[i].idade}`;
+                    li3.innerHTML = `Cor: ${arrayObj[i].Cor}`;
+                    li4.innerHTML = "Responsável: Túlio e Mariana!"
+                    main.appendChild(div);
+                    div.appendChild(section);
+                    section.appendChild(imagem);
+                    section.appendChild(ul);
+                    ul.appendChild(li1);
+                    ul.appendChild(li2);
+                    ul.appendChild(li3);
+                    ul.appendChild(li4);
+                    li1.appendChild(a); 
+                    
+
+                    
+            }; 
+
+        };
+    };
+    
+};
+
+
+
+
+
+//------------------semana 11-------------
+
+console.log(osMeusGatos);
+
+for (i in osMeusGatos){
+    let div = document.getElementById('div-sections');
+    let section = document.createElement("section");
+    let imagem = document.createElement("img");
+    let ul = document.createElement("ul");
+    let li1 = document.createElement ("li");
+    let li2 = document.createElement ("li");
+    let li3 = document.createElement ("li");
+    let li4 = document.createElement ("li");
+    let a = document.createElement ("a");
+    section.setAttribute("id","section1");
+    imagem.src = osMeusGatos[i].imagem;
+    a.setAttribute("href", osMeusGatos[i].link);
+    a.setAttribute("target","_blank");
+    a.innerHTML = osMeusGatos[i].nome.toUpperCase()
+    li1.innerHTML = `Nome: `
+    li2.innerHTML = `Idade: ${osMeusGatos[i].idade}`;
+    li3.innerHTML = `Cor: ${osMeusGatos[i].Cor}`;
+    li4.innerHTML = "Responsável: Túlio e Mariana!"
+    div.appendChild(section);
+    section.appendChild(imagem);
+    section.appendChild(ul);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+    ul.appendChild(li4);
+    li1.appendChild(a);  
+
+    
+
+};
+
+//-------------semana 12----------------
+
+function busca(event){
+    event.preventDefault()
+    let input = document.getElementById("input").value.toLowerCase()
+    if(input === ""){
+        alert("Campo de busca vazio!")
+    }else{
+        document.getElementById("div-sections").remove()
+        funcaoDeBusca(osMeusGatos, input);
+
+    }
+}
 
 
